661218549f20fdc002bd5888aa08dd766c377115
IrinaMolotkova <110341323+IrinaMolotkova@users.noreply.github.com>
2022-07-31 18:18:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,10 +8,3 @@ scrollTop:$("#" + $(this).data('value')).offset().top
 },1000)
 })
 
-function randomInt(min,max){
-return Math.floor(Math.random() * (max - min) + min);
-}
-
-alert(randomInt(10,20));
-
-let max = document.querySelector('input[name="max"]');
